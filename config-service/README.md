ee12dd5c002e234c0fb23a1a92c5bd2da11fac93
greyfolk <greyfolk99@gmail.com>
2023-04-02 12:30:02 +0900

    readme

---
+++ config-service/README.md
@@ -0,0 +1,4 @@
+## Config-Service
+
+- Spring Cloud Config로 설정 파일을 관리하는 서비스
+
---
+++ config-service/src/test/java/com/greyfolk99/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greyfolk99.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
